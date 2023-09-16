b797f9ee06e2431a6fcf9a08854b8cbd7a1128d9
RodrigoLF99 <rodrigofortess99@gemail.com>
2023-09-16 12:20:52 -0300

    fiz a criancao do endpoint createaluno

---
+++ BackEnd/AcaFormSystem/Controllers/AlunoController.cs
@@ -0,0 +1,20 @@
+﻿using AcaFormSystemAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcaFormSystemAPI.Controllers
+{  
+   [ApiController]
+   [Route("[controller]")]
+   public class AlunoController : ControllerBase
+   {
+
+        [HttpPost(Name = "CreateAluno")]
+        public ActionResult CreateAluno ( Aluno aluno)
+        {
+            return Ok(aluno);
+
+        }
+         
+
+        }
+}
